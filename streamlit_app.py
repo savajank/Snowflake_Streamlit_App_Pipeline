87f8ab1e43e82a4f0434c7a43074243644685053
Sava Jankovic <67196309+savajankovic-420@users.noreply.github.com>
2023-02-19 17:56:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#Craeting a pick list for the user to filter the table. 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
+#displaying the table on the page
 streamlit.dataframe(my_fruit_list)
