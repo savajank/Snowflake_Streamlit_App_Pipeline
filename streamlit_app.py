ecf367d4244b9c1432463e476d62e0a0b9228912
Sava Jankovic <67196309+savajankovic-420@users.noreply.github.com>
2023-02-19 18:00:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,8 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Creating a pick list for the user to filter the table. 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected] # the loc function finds the fruits
 
 #displaying the table on the page
 streamlit.dataframe(my_fruit_list)
