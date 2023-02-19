02bce1b4e11125e69e6ad856f378773f71f29db1
Sava Jankovic <67196309+savajankovic-420@users.noreply.github.com>
2023-02-19 17:25:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ streamlit.title("Hi guys my name is Sava")
 
 streamlit.header("Here are some things about me:")
 
-for i in range(5):
-  streamlit.text("I am a Computer Engineering student")
+streamlit.text("I am a Computer Engineering student")
+streamlit.text("I am Serbian")
+streamlit.text("I am an incoming Software and Data Analyst Intern for Sanofi")
+streamlit.text("I love coding and data!")
   
