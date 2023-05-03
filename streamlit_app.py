e117cf9225f2ad1846069a5213f6a0e91422c866
Sava Jankovic <67196309+savajankovic-420@users.noreply.github.com>
2023-05-03 16:38:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,5 +29,5 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 
-fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalize)
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
